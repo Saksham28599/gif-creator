29360fdf8b79b302cd3f9cdcf0adf6076464468a
Saksham Prakash Singh <prakashsaksham7@gmail.com>
2021-01-19 15:27:32 +0530

    heroku

---
+++ public/precache-manifest.05ad022ecfb0f9ad43c59cc203fa4a84.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a9f10d2dd03a5024d389f2cb7fcd2b3",
+    "url": "/gif-creator/index.html"
+  },
+  {
+    "revision": "cea5cd0a6625e3c3ef3c",
+    "url": "/gif-creator/static/css/main.cecd01fd.chunk.css"
+  },
+  {
+    "revision": "aaa0a14cd9217fea67cc",
+    "url": "/gif-creator/static/js/2.be59034a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/gif-creator/static/js/2.be59034a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e82cf817f786c65a896a",
+    "url": "/gif-creator/static/js/3.bccc3d1f.chunk.js"
+  },
+  {
+    "revision": "cea5cd0a6625e3c3ef3c",
+    "url": "/gif-creator/static/js/main.7306932c.chunk.js"
+  },
+  {
+    "revision": "eb194426973c8d5b02ec",
+    "url": "/gif-creator/static/js/runtime-main.e91aeaf4.js"
+  },
+  {
+    "revision": "24fba6bd89ce14b8218dd2ef85f5be72",
+    "url": "/gif-creator/static/media/Spinning_Ashoka_Chakra.24fba6bd.gif"
+  },
+  {
+    "revision": "77e7977fd7a91476ffea7a06db71f55a",
+    "url": "/gif-creator/static/media/error.77e7977f.gif"
+  }
+]);
--- src/utils/gifCreator.js
+++
@@ -1,8 +0,0 @@
-export const gifCreator = options =>
-    new Promise( (resolve, reject) => {
-        window.gifshot.createGIF(options, obj => {
-            !obj.error ?
-            resolve(obj.image) :
-            reject(obj.errorMsg)
-        })
-    })
